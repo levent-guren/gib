64536dc110f3e3f31364dc7538dd08fd717dc385
Levent GUREN <levent.guren@gmail.com>
2023-12-04 16:33:55 +0300

    projeler

--- IlkProje/src/deneme/Ornek9.java
+++ IlkProje/src/deneme/Ornek9.java
@@ -1,7 +1,22 @@
 package deneme;
 
+import java.util.Arrays;
+
 public class Ornek9 {
 	public static void main(String[] args) {
+		int[] sayilar = new int[5];
+		sayilar[1] = 3;
+		System.out.println(sayilar[0]);
+		System.out.println(sayilar);
 
+		for (int i = 0; i < sayilar.length; i++) {
+			System.out.println(sayilar[i]);
+		}
+		System.out.println("------------------");
+		for (int sayi : sayilar) { // foreach döngüsü
+			System.out.println(sayi);
+		}
+		System.out.println("------------------");
+		Arrays.stream(sayilar).forEach(System.out::println);
 	}
 }
